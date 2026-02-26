24dc9b0acdc68b87587e1e637da58d65723f9207
Luis <luis73web7@gmail.com>
2026-02-26 13:42:51 -0600

    Ejercicio 1 variables

---
+++ js/ejercicio1 tema variables.js
@@ -0,0 +1,12 @@
+// Ejercicio 1: Variables
+"use strict";
+// Declaración de variables 3 formas: var yano se usa, con let y const
+
+let nombre = "Juan"; 
+
+let="luis";
+console.log(nombre);
+
+
+
+
